ef27e6d6149078159671a6bd6e608f646d23bce1
Alivia Castor <soc11@georgetown.edu>
2025-04-15 22:44:05 -0400

    Built site for gh-pages

--- js/resolution_map.js
+++ js/resolution_map.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
+define(["d3"], function(d3) {
+    document.addEventListener("DOMContentLoaded", function () {
     console.log("✅ resolution_map.js loaded");
   
     const container = d3.select("#map-container");
@@ -118,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function () {
             
             // Redraw with new filter
             Promise.all([
-              d3.csv("../../311_map_data.csv"),
+              d3.csv("data/311_map_data.csv"),
               d3.json("data/SF_Find_Neighborhoods_2025.geojson")
             ]).then(([data, geo]) => {
               data.forEach(d => {
@@ -298,3 +299,4 @@ document.addEventListener("DOMContentLoaded", function () {
         .text("Resolution Rate");
     }
   });
+});
